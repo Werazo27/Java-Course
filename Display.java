881b6252676cff8420ea0d1070dadcbbe90bb302
Werazo27 <jlpriest27@gmail.com>
2025-01-22 23:37:46 -0500

    Data types

--- Display.java
+++ Display.java
@@ -1,7 +1,26 @@
 public class Display {
   public static void main(String[] args) {
-      System.out.println("Hello world");
+    int age = 27; //integer
+    System.out.println(age);
+
+    float temp = 82.6f; //floating point
+    System.out.println(temp);
+
+    double temperature = 43.28; //double floating point
+    System.out.println(temperature);
+
+    char grade = 'A'; //character
+    System.out.println(grade);
+    
+    String name = "Jebediah"; //string
+    System.out.println("Hello" + " " + name);
+    
+    boolean isLoggedIn = false; //boolean
+    System.out.println(isLoggedIn);
+    System.out.println(5<10);
+    System.out.println(5>10);
   }
+
 }
 
 //Single line comment
